b6d07f25143c6370214f37c2d526bafdbf3f0590
JinMori07 <griffith7207@gmail.com>
2026-05-27 18:25:04 +0200

    Depth first recursive

--- Model/RecursivePathFinder.cs
+++ Model/RecursivePathFinder.cs
@@ -8,33 +8,35 @@ namespace Model
 
         public void FindPath(Maze maze, int[] pos, Queue<int[]> visitedPositions) // akif
         {
-            bool[,] visisted = new bool[maze.MazeArray.Length, maze.MazeArray[0].Length]; // new 2d arrey with the same dimensions as the maze to keep track of which cell has been visisted
-            List<int[]> path = new List<int[]>();
-            Solve(maze, pos[0], pos[1], visisted, path); // enters the recursion starting from the first coordinate
-
-            foreach (var posloop in path) visitedPositions.Enqueue(posloop);
+            var path = new List<int[]>();
+            var visited = new HashSet<string>(); // to keep track of visited positions, using a hashset for O(1) lookup
+            if (DFS(maze, pos, path, visited))
+            {
+                foreach (var step in path)
+                {
+                    visitedPositions.Enqueue(step); // enqueue the steps in the path to the visitedPositions queue
+                }
+            }
         }
 
-        private bool Solve(Maze maze, int row, int col, bool[,] visited, List<int[]> path) // akif
+        private bool DFS(Maze maze, int[] pos, List<int[]> path, HashSet<string> visited)
         {
-            if (!maze.IsValidMove(row, col) || visited[row, col]) return false;  // if on a invalid location (out of bounds/already visited)
-            visited[row, col] = true; // keeping track of visisted
-            path.Add(new int[] {row, col});            
-
-            if (maze.MazeArray[row][col] == 2) // 2 : goal, if current coords are equal to the goal just return true
-                return true;
-
-            foreach (var move in maze.moves) // call solve for every possible move
+            if (!maze.IsValidMove(pos[0], pos[1])) return false; // if the move is invalid, return false
+            string key = $"{pos[0]},{pos[1]}"; // create a unique key for the current position
+            if (visited.Contains(key)) return false; // if we've already visited this position, return false
+            visited.Add(key); // mark the current position as visited
+            path.Add(pos); // add the current position to the path
+            if (pos[0] == maze.End[0] && pos[1] == maze.End[1]) return true; // if we've reached the end, return true
+            foreach (var move in maze.moves)
             {
-                int newRow = row + move[0];
-                int newCol = col + move[1];
-
-                if (Solve(maze, newRow, newCol, visited, path)) // if any of the called solve's return true return true
-                    return true;
+                if(DFS(maze, new int[] { pos[0] + move[0], pos[1] + move[1] }, path, visited))
+                {
+                    return true; // if the recursive call returns true, propagate that back up the call stack
+                }
             }
-
-            path.RemoveAt(path.Count - 1);
-            return false;
+            
+            path.RemoveAt(path.Count - 1); // remove the current position from the path
+            return false; // if no valid path is found, return false
         }
     }
 }
