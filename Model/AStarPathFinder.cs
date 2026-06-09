349a28921c5189b4c2d57fc3e32c8282ecff9153
Jaro592 <Jarogor5@gmail.com>
2026-06-09 23:57:46 +0200

    made some adjustments

--- Model/AStarPathFinder.cs
+++ Model/AStarPathFinder.cs
@@ -1,83 +1,95 @@
-
 namespace Model
 {
     public class AStarPathFinder : IPathFinder
     {
         PathFinderType _algType = PathFinderType.Astar;
         public PathFinderType algType { get => _algType; set {} }
+
         private string Key(int[] pos) => $"{pos[0]},{pos[1]}";
+
+        private int Heuristic(int[] pos, int[] end) =>
+            Math.Abs(pos[0] - end[0]) + Math.Abs(pos[1] - end[1]);
 
         public void FindPath(Maze maze, int[] pos, Queue<int[]> visitedPositions)
         {
             int[] start = pos;
             int[] end = maze.End;
 
-            var openset = new List<int[]>();
-            var cameFrom = new Dictionary<string, int[]>();
-            var gScore = new Dictionary<string, int>();
-            var fScore = new Dictionary<string, int>();
+            var unvisitedNodes = new List<int[]>();
+            var distance = new Dictionary<string, double>();
+            var prev = new Dictionary<string, int[]>();
 
-            var visited = new List<int[]>();
-            openset.Add(start);
-            gScore[Key(start)] = 0;
-            fScore[Key(start)] = Heuristic(start, end);
-
-            while (openset.Count > 0)
+            for (int r = 0; r < maze.MazeArray.Length; r++)
             {
-                int[] current = openset
-                    .OrderBy(n => fScore.GetValueOrDefault(Key(n), int.MaxValue)).First();
-                if (current.SequenceEqual(end))
+                for (int c = 0; c < maze.MazeArray[r].Length; c++)
                 {
-                    // Eerst alle verkende posities
-                    foreach (var v in visited)
-                        visitedPositions.Enqueue(v);
-
-                    // Dan het kortste pad
-                    var path = new Stack<int[]>();
-                    while (cameFrom.ContainsKey(Key(current)))
+                    if (maze.IsValidMove(r, c))
                     {
-                        path.Push(current);
-                        current = cameFrom[Key(current)];
-                    }
-                    path.Push(start);
-                    while (path.Count > 0)
-                        visitedPositions.Enqueue(path.Pop());
-
-                    return;
-                }
-
-                openset.Remove(current);
-                visited.Add(current);
-                foreach (var move in maze.moves)
-                {
-                    int newRow = current[0] + move[0];
-                    int newCol = current[1] + move[1];
-
-                    if (!maze.IsValidMove(newRow, newCol)) continue;
-
-                    int[] neighbor = new int[] { newRow, newCol };
-
-                    int tentativeG = gScore[Key(current)] + 1;
-                    if (!gScore.ContainsKey(Key(neighbor)) || tentativeG < gScore[Key(neighbor)])
-                    {
-                        cameFrom[Key(neighbor)] = current;
-                        gScore[Key(neighbor)] = tentativeG;
-                        fScore[Key(neighbor)] = tentativeG + Heuristic(neighbor, end);
-                        if (!openset.Any(n => n.SequenceEqual(neighbor)))
-                        {
-                            openset.Add(neighbor);
-                        }
+                        int[] node = new int[] { r, c };
+                        string key = Key(node);
+                        distance[key] = double.PositiveInfinity;
+                        prev[key] = null;
+                        unvisitedNodes.Add(node);
                     }
                 }
             }
 
+            distance[Key(start)] = 0;
 
+            while (unvisitedNodes.Count > 0)
+            {
+                int[] closestNode = unvisitedNodes[0];
+                foreach (var node in unvisitedNodes)
+                {
+                    double fNode = distance[Key(node)] + Heuristic(node, end);
+                    double fClosest = distance[Key(closestNode)] + Heuristic(closestNode, end);
+                    if (fNode < fClosest)
+                        closestNode = node;
+                }
+
+                if (distance[Key(closestNode)] == double.PositiveInfinity)
+                    break;
+
+                unvisitedNodes.Remove(closestNode);
+
+                if (closestNode.SequenceEqual(end))
+                    break;
+
+                foreach (var move in maze.moves)
+                {
+                    int newRow = closestNode[0] + move[0];
+                    int newCol = closestNode[1] + move[1];
+
+                    if (!maze.IsValidMove(newRow, newCol)) continue;
+
+                    int[] buur = new int[] { newRow, newCol };
+                    string buurKey = Key(buur);
+                    double difRoute = distance[Key(closestNode)] + 1;
+
+                    if (difRoute < distance[buurKey])
+                    {
+                        distance[buurKey] = difRoute;
+                        prev[buurKey] = closestNode;
+                    }
+                }
+            }
+
+            var path = new Stack<int[]>();
+            int[] current = end;
+
+            while (current != null && !current.SequenceEqual(start))
+            {
+                string currentKey = Key(current);
+                if (!prev.ContainsKey(currentKey) || prev[currentKey] == null)
+                    return;
+                path.Push(current);
+                current = prev[currentKey];
+            }
+
+            path.Push(start);
+
+            while (path.Count > 0)
+                visitedPositions.Enqueue(path.Pop());
         }
-
-    private int Heuristic(int[] pos, int[] end) =>
-        Math.Abs(pos[0] - end[0]) + Math.Abs(pos[1] - end[1]);
-
     }
 }
-
-            
--- Model/DijkstraPathFinder.cs
+++ Model/DijkstraPathFinder.cs
@@ -4,67 +4,87 @@ namespace Model
     {
         PathFinderType _algType = PathFinderType.Dijkstra;
         public PathFinderType algType { get => _algType; set {} }
+
         private string Key(int[] pos) => $"{pos[0]},{pos[1]}";
+
         public void FindPath(Maze maze, int[] pos, Queue<int[]> visitedPositions)
         {
-            // To do: Implement Dijkstra's algorithm
             int[] start = pos;
             int[] end = maze.End;
-            //var visited = new List<int[]>();
 
-            var openset = new List<int[]>();
-            var cameFrom = new Dictionary<string, int[]>();
-            var gScore = new Dictionary<string, int>();
-            openset.Add(start);
-            gScore[Key(start)] = 0;
+            var unvisitedNodes = new List<int[]>();
+            var distance = new Dictionary<string, double>();
+            var prev = new Dictionary<string, int[]>();
 
-            while (openset.Count > 0)
+            for (int r = 0; r < maze.MazeArray.Length; r++)
             {
-                int[] current = openset.OrderBy(n => gScore.GetValueOrDefault(Key(n), int.MaxValue)).First();
-
-                if (current.SequenceEqual(end))
+                for (int c = 0; c < maze.MazeArray[r].Length; c++)
                 {
-                    //foreach (var v in visited)
-                    //{
-                    //    visitedPositions.Enqueue(v);
-                    //}
-                    var path = new Stack<int[]>();
-                    while (cameFrom.ContainsKey(Key(current)))
+                    if (maze.IsValidMove(r, c))
                     {
-                        path.Push(current);
-                        current = cameFrom[Key(current)];
-                    }
-                    path.Push(start);
-                    while (path.Count > 0)
-                    {
-                        visitedPositions.Enqueue(path.Pop());
-                    }
-                    return;
-                }
-
-                openset.Remove(current);
-                //visited.Add(current); 
-                foreach (var move in maze.moves)
-                {
-                    int newRow = current[0] + move[0];
-                    int newCol = current[1] + move[1];
-
-                    if (!maze.IsValidMove(newRow, newCol)) continue;
-
-                    int[] neighbor = new int[] { newRow, newCol };
-
-                    int tentativeG = gScore[Key(current)] + 1;
-                    if (!gScore.ContainsKey(Key(neighbor)) || tentativeG < gScore[Key(neighbor)])
-                    {
-                        cameFrom[Key(neighbor)] = current;
-                        gScore[Key(neighbor)] = tentativeG;
-                        if (!openset.Any(n => n.SequenceEqual(neighbor)))
-                        {
-                            openset.Add(neighbor);
-                        }
+                        int[] node = new int[] { r, c };
+                        string key = Key(node);
+                        distance[key] = double.PositiveInfinity;
+                        prev[key] = null;
+                        unvisitedNodes.Add(node);
                     }
                 }
             }
+
+            distance[Key(start)] = 0;
+
+            while (unvisitedNodes.Count > 0)
+            {
+                int[] closestNode = unvisitedNodes[0];
+                foreach (var node in unvisitedNodes)
+                {
+                    if (distance[Key(node)] < distance[Key(closestNode)])
+                        closestNode = node;
+                }
+
+                if (distance[Key(closestNode)] == double.PositiveInfinity)
+                    break;
+
+                unvisitedNodes.Remove(closestNode);
+
+                if (closestNode.SequenceEqual(end))
+                    break;
+
+                foreach (var move in maze.moves)
+                {
+                    int newRow = closestNode[0] + move[0];
+                    int newCol = closestNode[1] + move[1];
+
+                    if (!maze.IsValidMove(newRow, newCol)) continue;
+
+                    int[] buur = new int[] { newRow, newCol };
+                    string buurKey = Key(buur);
+                    double difRoute = distance[Key(closestNode)] + 1;
+
+                    if (difRoute < distance[buurKey])
+                    {
+                        distance[buurKey] = difRoute;
+                        prev[buurKey] = closestNode;
+                    }
+                }
+            }
+
+            var path = new Stack<int[]>();
+            int[] current = end;
+
+            while (current != null && !current.SequenceEqual(start))
+            {
+                string currentKey = Key(current);
+                if (!prev.ContainsKey(currentKey) || prev[currentKey] == null)
+                    return;
+                path.Push(current);
+                current = prev[currentKey];
+            }
+
+            path.Push(start);
+
+            while (path.Count > 0)
+                visitedPositions.Enqueue(path.Pop());
         }
-   }
+    }
 }
